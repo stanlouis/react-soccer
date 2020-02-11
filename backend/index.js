ed11b1a190e09f18be0aa17e26df6df04f04e930
Stanley Louis <stanlouis@msn.com>
2020-02-11 13:17:43 -0500

    Add database connection and player schema

--- backend/index.js
+++ backend/index.js
@@ -1,11 +1,23 @@
 import express from 'express';
 import helmet from 'helmet';
 import morgan from 'morgan';
+import mongoose from 'mongoose';
 
 const app = express();
 app.use(helmet());
 app.use(morgan('tiny'));
-const PORT = process.env.PORT || 3000;
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+const PORT = process.env.PORT || 4500;
+
+// mongo connection
+mongoose
+  .connect('mongodb://localhost/soccerDB', {
+    useNewUrlParser: true,
+    useUnifiedTopology: true
+  })
+  .then(() => console.log('Connected to MongoDB...'))
+  .catch(err => console.error('Could not connect to mongoDB'));
 
 app.get('/', (req, res) => {
   res.send(`Our Soccer application is running on port ${PORT}`);
