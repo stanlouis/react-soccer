d539c810ee58013605549cbe4e0ab6a145f603ad
Stanley Louis <stanlouis@msn.com>
2020-02-11 22:24:52 -0500

    Add state and fetch players with axios.

--- frontend/src/Components/App.js
+++ frontend/src/Components/App.js
@@ -1,30 +1,58 @@
-import React from 'react';
+import React, { Component } from 'react';
+import axios from 'axios';
 import './App.css';
 import PlayerSingle from './Player/PlayerSingle';
 import PlayerList from './Player/PlayerList';
 import PlayerForm from './Player/PlayerForm';
 
-function App() {
-  return (
-    <div className="container-fluid">
-      <div className="row">
-        <div className="col s12">Menu</div>
-      </div>
-      <div className="row">
-        <div className="col s3">
-          <PlayerList />
+class App extends Component {
+  state = {
+    players: [],
+    currentPlayer: {}
+  };
+
+  componentDidMount() {
+    const url = 'http://localhost:4500/players';
+
+    axios
+      .get(url)
+      .then(response => {
+        this.setState({ players: response.data });
+      })
+      .catch(error => console.error(error));
+  }
+
+  updateCurrentPlayer = item => {
+    this.setState({
+      currentPlayer: item
+    });
+  };
+
+  render() {
+    return (
+      <div className="container-fluid">
+        <div className="row">
+          <div className="col s12">Menu</div>
         </div>
-        <div className="col s9">
-          <PlayerSingle />
+        <div className="row">
+          <div className="col s3">
+            <PlayerList
+              players={this.state.players}
+              updateCurrentPlayer={this.updateCurrentPlayer}
+            />
+          </div>
+          <div className="col s9">
+            <PlayerSingle />
+          </div>
+        </div>
+        <div className="row">
+          <div className="col s12">
+            <PlayerForm />
+          </div>
         </div>
       </div>
-      <div className="row">
-        <div className="col s12">
-          <PlayerForm />
-        </div>
-      </div>
-    </div>
-  );
+    );
+  }
 }
 
 export default App;
